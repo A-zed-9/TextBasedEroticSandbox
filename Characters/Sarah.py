7a6abee0c09da37acef2e3f26c5b06352189df12
A-zed-9 <85193216+A-zed-9@users.noreply.github.com>
2021-06-01 15:16:33 -0400

    Add files via upload

---
+++ Characters/Sarah.py
@@ -0,0 +1,21 @@
+# Sarah
+
+# Dialogue
+dialogue_intro = "Hey there! What did you want to talk about?"
+dialogue_ask_to_continue = "Was there something else you wanted to talk about?"
+dialogue_continue = "What else did you want to talk about?"
+dialogue_dictionary = {
+    "Weekend": {
+        "True": "print(\"I like to spend my weekends out, I'm sure you'll catch me at one of the bars around here if you like "
+                "to dance.\") ",
+    },
+    "Hold Hands": {
+        "self.friendship >= 10 or self.romance >= 5": 'print(\"I\'d love to hold your hand!\")\nself.hold_hands()',
+        "True": "print(\"I'd rather not hold your hand.\")"
+    }
+}
+# Stats
+stats_dictionary = {
+    "friendship": 10,
+    "romance": 0
+}
