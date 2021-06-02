3cd77a197c7b95185a1c1e08428f02a9476a6313
A-zed-9 <85193216+A-zed-9@users.noreply.github.com>
2021-06-02 14:21:39 -0400

    Add files via upload

--- Characters/Sarah.py
+++ Characters/Sarah.py
@@ -6,12 +6,13 @@ dialogue_ask_to_continue = "Was there something else you wanted to talk about?"
 dialogue_continue = "What else did you want to talk about?"
 dialogue_dictionary = {
     "Weekend": {
-        "True": "print(\"I like to spend my weekends out, I'm sure you'll catch me at one of the bars around here if you like "
-                "to dance.\") ",
+        "True": "print(\"I like to spend my weekends out, I'm sure you'll catch me at one of the bars around here if "
+                "you like to dance.\") ",
     },
     "Holding Hands": {
-        "self.friendship >= 10 or self.romance >= 5": 'print(\"I\'d love to hold your hand!\")\nself.hold_hands()',
-        "True": "print(\"I'd rather not hold your hand.\")"
+        "self.friendship >= 10 or self.romance >= 5": "print(\"I\'d love to hold your hand!\")\nself.Holding_Hands("
+                                                      "approval=True)",
+        "True": "print(\"I'd rather not hold your hand.\")\nself.Holding_Hands(approval=False)"
     }
 }
 # Stats
--- Main.py
+++ Main.py
@@ -33,7 +33,8 @@ def interpret():
     arguments = []
     dictionary = {
         "Keyword": ["List of arguments lists"],
-        "Talk": ["dialogue_topics"]
+        "Talk": ["dialogue_topics"],
+        "Stop": ["methods"]
     }
     while True:
         answer = user_input_to_list("What would you like to do?")
@@ -44,9 +45,9 @@ def interpret():
                 subject = item
         if keyword and subject:
             for item in answer:
-                for list in dictionary[keyword]:
-                    if type(list) is str:
-                        if item in eval(subject + '.' + list):
+                for x in dictionary[keyword]:
+                    if type(x) is str:
+                        if item in eval(subject + '.' + x):
                             arguments.append(item)
             try:
                 return exec(subject + "." + keyword + "(" + "*" + str(arguments) + ")")
@@ -57,8 +58,8 @@ def interpret():
 def user_input_to_list(prompt):
     input_list = input(prompt).split()
     for index, item in enumerate(input_list):
-        if '-' in item:
-            new_item = item.replace('-', ' ')
+        if '.' or '!' or '?' in item:
+            new_item = item.strip('.!?')
             input_list = [new_item if x == input_list[index] else x for x in input_list]
     return input_list
 
@@ -81,7 +82,10 @@ class NPC:
         for stat in self.stats_dictionary:
             exec('self.' + stat + ' = ' + str(self.stats_dictionary[stat]))
         # Sates
-        self.holding_hands = "False"
+        self.holding_hands_state = False
+        # Methods
+        self.methods = [method for method in dir(NPC) if method.startswith('__') is False]
+        self.methods.remove("Stop")
 
     def Talk(self, topic):
         for condition in self.dialogue_dictionary[topic]:
@@ -90,22 +94,28 @@ class NPC:
                 if check_input(self.dialogue_ask_to_continue, yesno) == "Yes":
                     topic = check_input(self.dialogue_continue, self.dialogue_topics)
                     self.Talk(topic=topic)
+                else:
+                    return
 
-    def hold_hands(self, approval=None, stop=None):
-        while True:
-            if self.holding_hands and not stop:
-                return print("You are already holding hands with " + self.name)
-            elif self.holding_hands and stop:
-                return print("You are no longer holding hands with " + self.name)
-            elif not self.holding_hands:
-                if approval:
-                    return print("You are now holding hands with " + self.name + ".")
-                elif not approval:
-                    if check_input("Do it anyway?", yesno) == "Yes":
-                        print("You grab " + self.name + "'s hand she tries to move it away but you hold on.")
-                    else:
-                        return print("You don't hold hands with " + self.name)
+    def Holding_Hands(self, approval=None, stop=None):
+        if self.holding_hands_state == True and not stop:
+            return print("You are already holding hands with " + self.name)
+        elif self.holding_hands_state == False and stop:
+            return print("You are no longer holding hands with " + self.name)
+        elif not self.holding_hands_state and stop:
+            return print("You aren't holding " + self.name +"'s hand.")
+        elif not self.holding_hands_state and not stop:
+            if approval:
+                return print("You are now holding hands with " + self.name + ".")
+            elif not approval:
+                if check_input("Do it anyway?", yesno) == "Yes":
+                    self.holding_hands_state = True
+                    return print("You grab " + self.name + "'s hand she tries to move it away but you hold on.")
+                else:
+                    return print("You don't hold hands with " + self.name)
 
+    def Stop(self, function):
+        exec("self." + function + "(stop=True)")
 
 initialize_characters()
 interpret()
